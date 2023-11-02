89a4a4cb6b61ec1e950db453573b3c4c91d34e03
Ayame Hinode <ayamehinode@gmail.com>
2023-11-01 21:01:47 -0300

    About system output and how to format output

---
+++ Section4/Operadores.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class Operadores {
+
+    public static void main(String [] args){
+        
+        int y = 32;
+        System.out.println(y);
+
+        System.out.println("Bom dia!");
+
+        double x = 10.37524;
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.3f%n", x);
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.2f%n", x);
+        System.out.println("Resultado: " + x + " metros.");
+        System.out.printf("Resultado: %.2f metros%n", x);
+
+        String nome = "Ayame";
+        int idade = 18;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+    }
+
+}
