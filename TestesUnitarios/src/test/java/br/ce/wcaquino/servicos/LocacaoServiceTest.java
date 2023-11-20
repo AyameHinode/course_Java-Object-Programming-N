4f0ff40cb1cafd1359547f494a46358aab78d785
Ayame Hinode <ayamehinode@gmail.com>
2023-11-19 21:55:41 -0300

    Sorting tests

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -27,23 +27,7 @@ public class LocacaoServiceTest {
 
     @Before
     public void setup() {
-        System.out.println("Before Method");
         locacaoService = new LocacaoService();
-    }
-
-    @After
-    public void tearDown(){
-        System.out.println("After Method");
-    }
-
-    @BeforeClass
-    public static void setupClass() {
-        System.out.println("Before Class");
-    }
-
-    @AfterClass
-    public static void tearDownClass(){
-        System.out.println("After Class");
     }
 
     @Test
