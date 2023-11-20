e6963c4dd93a8f5663f106fa9a1785e28d77a766
Ayame Hinode <ayamehinode@gmail.com>
2023-11-20 13:53:21 -0300

    Calculadora created with TDD method

---
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,64 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+    private Calculadora calculadora;
+
+    @Before
+    public void setup(){
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void deveSomarDoisValores(){
+        //cenario
+        int a = 2;
+        int b = 4;
+
+        //acao
+        int resultado = calculadora.somar(a, b);
+
+        //verificacao
+        Assert.assertEquals(6, resultado);
+    }
+
+    @Test
+    public void deveSubtrairDoisValores(){
+        //cenario
+        int a = 10;
+        int b = 7;
+
+        //acao
+        int resultado = calculadora.subtrair(a, b);
+
+        //verificacao
+        Assert.assertEquals(3, resultado);
+    }
+
+    @Test
+    public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+        //cenario
+        int a = 15;
+        int b = 5;
+
+        //acao
+        int resultado = calculadora.dividir(a, b);
+
+        //verificacao
+        Assert.assertEquals(3, resultado);
+    }
+
+    @Test(expected = NaoPodeDividirPorZeroException.class)
+    public void deveLancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+        int a = 10;
+        int b = 0;
+
+        calculadora.dividir(a, b);
+    }
+
+}
