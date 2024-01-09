e0aa8c3ad595d170edf0845b427ed48c64945a9e
Ayame Hinode <ayamehinode@gmail.com>
2024-01-09 20:59:13 -0300

    About bitwise operators

---
+++ Section7-OtherTopics/classes/Bitwise.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Bitwise {
+
+    static public void main (String ... args) {
+
+        int n1 = 90;
+        int n2 = 200;
+
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+
+
+    }
+}
