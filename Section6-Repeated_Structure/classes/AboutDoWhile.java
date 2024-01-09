15396105d36137e0c79f1cc70425aabc553507df
Ayame Hinode <ayamehinode@gmail.com>
2024-01-08 22:40:55 -0300

    About do while

--- Section6-Repeated_Structure/classes/AboutDoWhile.java
+++ Section6-Repeated_Structure/classes/AboutDoWhile.java
@@ -10,14 +10,16 @@ public class AboutDoWhile {
         Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite a temperatura em Celsius: ");
-        double C = entrada.nextDouble();
-        double F = 9.0 * C / 5.0 + 32.0;
-        System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-        System.out.print("Deseja repetir (s/n)? ");
-        char resp = entrada.next().charAt(0);
-
-        while (resp != 'n')
+        char resp;
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = entrada.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)? ");
+            resp = entrada.next().charAt(0);
+        } while (resp != 'n');
+        entrada.close();
     }
 
 }
