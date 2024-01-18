8522f21ca110c439ae0d1a76dd557288c0c99998
Ayame Hinode <ayamehinode@gmail.com>
2024-01-18 19:58:25 -0300

    Implementing To String

--- Section8- Object-oriented programming/Application2/application/Program.java
+++ Section8- Object-oriented programming/Application2/application/Program.java
@@ -1,2 +1,30 @@
-package Application2.application;public class Program {
+package Application2.application;
+
+import Application2.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+        Product produto = new Product();
+        System.out.println("Entre com os dados do produto: ");
+        System.out.print("Nome: ");
+        produto.nome = entrada.nextLine();
+        System.out.print("Preço: ");
+        produto.preco = entrada.nextDouble();
+        System.out.print("Quantidade em estoque: ");
+        produto.quantidade = entrada.nextInt();
+
+        System.out.println(produto.toString());
+
+        entrada.close();
+
+    }
+
 }
--- Section8- Object-oriented programming/Application2/entities/Product.java
+++ Section8- Object-oriented programming/Application2/entities/Product.java
@@ -1,2 +1,31 @@
-package Application2.entities;public class Product {
+package Application2.entities;
+
+public class Product {
+
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public double valorTotalEmEstoque() {
+        return preco * quantidade;
+    }
+
+    public void adicionarProdutos(int quantidade){
+        this.quantidade += quantidade;
+    }
+
+    public void removerProdutos(int quantidade){
+        this.quantidade -= quantidade;
+    }
+
+    public String toString(){
+        return nome
+                + ", $ "
+                + String.format("%.2f", preco)
+                +", "
+                + quantidade
+                + " unidades, Total: $ "
+                + String.format("%.2f", valorTotalEmEstoque());
+    }
+
 }
